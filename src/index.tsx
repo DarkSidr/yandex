84945d6bd9993ebca4b53cd6acd9ed652e41ea6c
Kamil Vakhitov <kamil.vakhitov95@gmail.com>
2023-10-27 06:01:39 +0500

    delete basename

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename="https://darksidr.github.io/yandex/">
+      <HashRouter>
         <App />
       </HashRouter>
     </Provider>
